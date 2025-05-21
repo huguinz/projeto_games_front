1c72ed9619f128cd81b431789b988a83bd36f066
huguinz <hugolopessouza58@gmail.com>
2025-05-21 20:27:42 -0300

    syncing project to remote repository

---
+++ scripts/home/js/main.js
@@ -0,0 +1,6 @@
+const teste = document.getElementById('checkbox_switch')
+const teste2 = document.getElementById('checkbox_label')
+
+teste2.addEventListener('click', () => {
+	console.log(teste.checked)
+})
